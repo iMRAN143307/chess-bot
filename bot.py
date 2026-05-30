dad26a8c4e11f338dc750a0a3c7ad99d8b42c39f
iMRAN143307 <imranmawji2@gmail.com>
2026-05-30 17:26:18 -0400

    finished stage 3

--- bot.py
+++ bot.py
@@ -6,8 +6,12 @@ come back to this in Week 3 when we integrate UCI and submit to the tournament.
 
 from __future__ import annotations
 
+from random import choice
+
+from chessdk import Move, min_attacker_value
+from chessdk.evaluation import PIECE_VALUE
+
 from board import Board
-from chessdk import Move
 
 
 def choose_move(board: Board, time_left_ms: int) -> Move:
@@ -16,4 +20,26 @@ def choose_move(board: Board, time_left_ms: int) -> Move:
     `time_left_ms` is how many milliseconds you have remaining in the match.
     For Week 1 this function is unused; later weeks replace it with real logic.
     """
-    raise NotImplementedError("choose_move: implement me in Week 3")
+
+    def score(m: Move):
+        pre_score = PIECE_VALUE[board.piece_at(m.to_sq).kind]
+        board.make_move(m)
+        if board.is_attacked(m.to_sq, board.state.side_to_move):
+            min_val = (
+                min_attacker_value(board, m.to_sq, board.state.side_to_move)
+                if min_attacker_value(board, m.to_sq, board.state.side_to_move)
+                is not None
+                else 2001
+            )
+            if min_val < PIECE_VALUE[board.piece_at(m.to_sq).kind]:
+                pre_score = pre_score - PIECE_VALUE[board.piece_at(m.to_sq).kind]
+        board.undo_move()
+        return pre_score
+
+    moves = board.legal_moves()
+    captures = [m for m in moves if board.piece_at(m.to_sq) is not None]
+    if captures:
+        scored_captures = [score(m) for m in captures]
+        return captures[scored_captures.index(max(scored_captures))]
+    else:
+        return choice(moves)
